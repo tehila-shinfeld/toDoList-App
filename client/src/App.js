e7cfee0339fe0e44c3ac26266c5bd84e33e3ae8f
username <useremail>
2025-02-04 22:25:49 +0200

    temp8

--- client/src/App.js
+++ client/src/App.js
@@ -34,8 +34,7 @@ function App() {
   return (
     <section className="todoapp">
       <header className="header">
-        <h1>todos</h1>
-        <h2>By Tehils Shinfeld</h2>
+        <h1>todos - By Tehils Sh.  💙</h1>
         <form onSubmit={createTodo}>
           <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
         </form>
